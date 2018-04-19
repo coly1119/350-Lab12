a2839a7de212b24134f314d17c42722d82612dbd
Andy Liu <coly1119@Andys-MacBook-Pro.local>
2018-04-19 17:16:15 -0400

    2b model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -2,5 +2,17 @@ generate_data = function(n, p)
 {
   covariates = matrix(data = rnorm(n*p, 0, 1), nrows = n, ncols = p)
   responses = rnorm(n, 0, 1)
-  return(list(covariates = covariates, responses = responses))
+  return(list(covariates, responses))
+}
+
+model_select = function(covariates, responses, cutoff)
+{
+  lm1 = lm(responses~covariates)
+  if(cutoff < 1) 
+  {
+    lm2 = lm(responses~c())
+  }
+  else{
+    lm2 = lm(responses~covraiates[,1:cutoff])
+  }
 }
